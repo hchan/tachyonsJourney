ba5b80745f0d6b8abcd5f86d8ab480b9f6d216da
Henry Chan <henry.chan@trefis.com>
2015-03-21 16:22:21 -0700

    a

--- core/src/com/suitecompiletech/tachyonsjourney/screen/BirthStarScreen.java
+++ core/src/com/suitecompiletech/tachyonsjourney/screen/BirthStarScreen.java
@@ -1,94 +1,49 @@
 package com.suitecompiletech.tachyonsjourney.screen;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL30;
-import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.badlogic.gdx.utils.TimeUtils;
-import com.suitecompiletech.tachyonsjourney.sprite.BlueBall;
-import com.suitecompiletech.tachyonsjourney.sprite.RedBall;
+import com.suitecompiletech.tachyonsjourney.sprite.HydrogenSprite;
 import com.suitecompiletech.tachyonsjourney.sprite.TachyonWatching;
 
 public class BirthStarScreen extends BaseScreen {
 	private TachyonWatching tachyonWatching = new TachyonWatching();
-	private float multiplier = 1.01f;
-	
+	private HydrogenSprite hydrogenSprite1 = new HydrogenSprite();
+
 	@Override
 	public void render(float delta) {
 		super.render(delta);
 		Sprite sprite = null;
-		multiplier *= 1.01f;
-		
-		//if (TimeUtils.timeSinceMillis(startTime) > 10000) {
-			TextureRegion currentFrame = tachyonWatching.getAnimation()
-					.getKeyFrame(1, true);
-			float width = Gdx.graphics.getWidth()/10;
-			float height = Gdx.graphics.getWidth()/10;
-			float x = (Gdx.graphics.getWidth()-width)/8;
-			float y = (Gdx.graphics.getHeight()-2*height);
-			
-		
-			sprite = new Sprite(currentFrame);
-			sprite.setX(x);
-			sprite.setY(y);
-			sprite.setSize(width, height);
-			//sprite.setRotation(90f * multiplier);
-			sprite.setOrigin(width/2, height/2);
-			spriteBatch.begin();
-			sprite.draw(spriteBatch);
-			
-			//multiplier *= (TimeUtils.timeSinceMillis(startTime)/10000) * 1.01;
 
-		//}
-			
-			
-			RedBall redBall = new RedBall();
-			width = Gdx.graphics.getWidth()/20;
-			height = Gdx.graphics.getWidth()/20;
-			redBall.setSize(width, height);
-			redBall.setOriginCenter();//(width/2, height/2);
-			redBall.setX((Gdx.graphics.getWidth()-redBall.getWidth())/2);
-			redBall.setY((Gdx.graphics.getHeight()-redBall.getHeight())/2);
-			float rotation = 90f * multiplier;
-			if (multiplier > 1.5) {
-				multiplier = 1.01f;
-			}
-			redBall.setRotation(rotation);
-			
-		
-		
-			
-			BlueBall blueBall = new BlueBall();
-			width = Gdx.graphics.getWidth()/80;
-			height = Gdx.graphics.getWidth()/80;
-			blueBall.setSize(width, height);
-			blueBall.setOriginCenter();
-			blueBall.setX((Gdx.graphics.getWidth()-blueBall.getWidth())/2);
-			blueBall.setY((Gdx.graphics.getHeight()-blueBall.getHeight())/2);
-			blueBall.setRotation(rotation);
-			float theta = (float) (stateTime*2 % 2*Math.PI);
-			updateSpriteXYForEllipse(theta, redBall.getX() + redBall.getOriginX(), redBall.getY() + redBall.getOriginY(), 80, 40, blueBall);
-			
-		if (theta > 0.5 * Math.PI && theta < 1.5* Math.PI) {
-			blueBall.draw(spriteBatch);
-			redBall.draw(spriteBatch);
-		} else {
-			redBall.draw(spriteBatch);
-			blueBall.draw(spriteBatch);
-		}
-			
-			//spriteBatch.draw(redBall, (Gdx.graphics.getWidth()-redBall.getWidth())/2, (Gdx.graphics.getHeight()-redBall.getHeight())/2);
-			spriteBatch.end();
+		// if (TimeUtils.timeSinceMillis(startTime) > 10000) {
+		TextureRegion currentFrame = tachyonWatching.getAnimation()
+				.getKeyFrame(1, true);
+		float width = Gdx.graphics.getWidth() / 10;
+		float height = Gdx.graphics.getWidth() / 10;
+		float x = (Gdx.graphics.getWidth() - width) / 8;
+		float y = (Gdx.graphics.getHeight() - 2 * height);
+
+		sprite = new Sprite(currentFrame);
+		sprite.setX(x);
+		sprite.setY(y);
+		sprite.setSize(width, height);
+		// sprite.setRotation(90f * multiplier);
+		sprite.setOrigin(width / 2, height / 2);
+		spriteBatch.begin();
+		sprite.draw(spriteBatch);
+		hydrogenSprite1.render(stateTime);
+		hydrogenSprite1.draw(spriteBatch);
+		hydrogenSprite1.getRedBall().setX(hydrogenSprite1.getRedBall().getX()+1);
+		hydrogenSprite1.getRedBall().setY(hydrogenSprite1.getRedBall().getY()+1);
+		spriteBatch.end();
 	}
 	
 	
-	  public void updateSpriteXYForEllipse(float theta, float xCenter, float yCenter, float xRadius, float yRadius, Sprite sprite) {
-		  float x = (float) (xCenter + xRadius*Math.cos(theta));
-		  float y = (float) (yCenter - yRadius*Math.sin(theta));
-		  x -= sprite.getWidth()/2;
-		  y -= sprite.getHeight()/2;
-		  sprite.setX(x);
-		  sprite.setY(y);
-	   }
+	@Override
+	public void show() {
+		super.show();
+		hydrogenSprite1.getRedBall().setX(0);
+		hydrogenSprite1.getRedBall().setY(0);
+	}
+	
 }
--- core/src/com/suitecompiletech/tachyonsjourney/sprite/HydrogenSprite.java
+++ core/src/com/suitecompiletech/tachyonsjourney/sprite/HydrogenSprite.java
@@ -1,43 +1,125 @@
 package com.suitecompiletech.tachyonsjourney.sprite;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import java.util.Random;
 
 public class HydrogenSprite extends Sprite {
 	private RedBall redBall;
 	private BlueBall blueBall;
 	private float multiplier = 1.01f;
-	
+	private float theta;
+	private float velocity;
+	private Random rand = new Random();
+	private float ellipseRotation;
+
 	public HydrogenSprite() {
+
+		velocity = rand.nextFloat() * 2 + 1.5f;
+		ellipseRotation = (float) (rand.nextFloat() * 2 * Math.PI);
 		float width, height;
 		redBall = new RedBall();
-		width = Gdx.graphics.getWidth()/20;
-		height = Gdx.graphics.getWidth()/20;
+		width = Gdx.graphics.getWidth() / 40;
+		height = Gdx.graphics.getWidth() / 40;
 		redBall.setSize(width, height);
-		redBall.setOriginCenter();//(width/2, height/2);
-		redBall.setX((Gdx.graphics.getWidth()-redBall.getWidth())/2);
-		redBall.setY((Gdx.graphics.getHeight()-redBall.getHeight())/2);
-		
-	
-	
-		
-		 blueBall = new BlueBall();
-		width = Gdx.graphics.getWidth()/80;
-		height = Gdx.graphics.getWidth()/80;
+		redBall.setOriginCenter();// (width/2, height/2);
+		redBall.setX((Gdx.graphics.getWidth() - redBall.getWidth()) / 2);
+		redBall.setY((Gdx.graphics.getHeight() - redBall.getHeight()) / 2);
+
+		blueBall = new BlueBall();
+		width = Gdx.graphics.getWidth() / 80;
+		height = Gdx.graphics.getWidth() / 80;
 		blueBall.setSize(width, height);
 		blueBall.setOriginCenter();
-		blueBall.setX((Gdx.graphics.getWidth()-blueBall.getWidth())/2);
-		blueBall.setY((Gdx.graphics.getHeight()-blueBall.getHeight())/2);
-	
+		blueBall.setX((Gdx.graphics.getWidth() - blueBall.getWidth()) / 2);
+		blueBall.setY((Gdx.graphics.getHeight() - blueBall.getHeight()) / 2);
+
 	}
-	
-	public void render() {
+
+	@Override
+	public void draw(Batch batch) {
+		if (theta > 0.5 * Math.PI && theta < velocity * Math.PI) {
+			blueBall.draw(batch);
+			redBall.draw(batch);
+		} else {
+			redBall.draw(batch);
+			blueBall.draw(batch);
+		}
+	}
+
+	public void render(float stateTime) {
+		theta = (float) (stateTime * 2 % 2 * Math.PI);
+		updateSpriteXYForEllipse(theta, redBall.getX() + redBall.getOriginX(),
+				redBall.getY() + redBall.getOriginY(), 80, 40, blueBall);
+
 		float rotation = 90f * multiplier;
 		if (multiplier > 1.5) {
 			multiplier = 1.01f;
 		}
 		redBall.setRotation(rotation);
 		blueBall.setRotation(rotation);
-		
+
 	}
+
+	// http://www.uwgb.edu/dutchs/Geometry/HTMLCanvas/ObliqueEllipses5.HTM
+	public void updateSpriteXYForEllipse(float theta, float xCenter,
+			float yCenter, float xRadius, float yRadius, Sprite sprite) {
+		float x = (float) (xCenter + ((xRadius * Math.cos(theta)) * Math.cos(ellipseRotation) - yRadius * Math.sin(theta) * Math.sin(ellipseRotation)));
+		float y = (float) (yCenter - ((yRadius * Math.sin(theta)) * Math.cos(ellipseRotation) + xRadius * Math.cos(theta) * Math.sin(ellipseRotation)));
+		x -= sprite.getWidth() / 2;
+		y -= sprite.getHeight() / 2;
+		sprite.setX(x);
+		sprite.setY(y);
+	}
+
+	public RedBall getRedBall() {
+		return redBall;
+	}
+
+	public void setRedBall(RedBall redBall) {
+		this.redBall = redBall;
+	}
+
+	public BlueBall getBlueBall() {
+		return blueBall;
+	}
+
+	public void setBlueBall(BlueBall blueBall) {
+		this.blueBall = blueBall;
+	}
+
+	public float getMultiplier() {
+		return multiplier;
+	}
+
+	public void setMultiplier(float multiplier) {
+		this.multiplier = multiplier;
+	}
+
+	public float getTheta() {
+		return theta;
+	}
+
+	public void setTheta(float theta) {
+		this.theta = theta;
+	}
+
+	public float getVelocity() {
+		return velocity;
+	}
+
+	public void setVelocity(float velocity) {
+		this.velocity = velocity;
+	}
+
+	public Random getRand() {
+		return rand;
+	}
+
+	public void setRand(Random rand) {
+		this.rand = rand;
+	}
+	
+	
 }
