1b87272af8a8f02cc1ba0bdebabdffec15d7d2b2
hchan <hchan@apache.org>
2015-03-25 20:11:29 -0700

    a

---
+++ core/src/com/suitecompiletech/tachyonsjourney/sprite/HydrogenPairSprite.java
@@ -0,0 +1,12 @@
+package com.suitecompiletech.tachyonsjourney.sprite;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.Batch;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import java.util.Random;
+
+public class HydrogenPairSprite extends SpritePlus {
+	private HydrogenSprite hydrogenSprite1;
+	private HydrogenSprite hydrogenSprite2;
+	
+}
