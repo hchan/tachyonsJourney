fb290ce89344bb1b02245764cec8206686cf81a6
Henry Chan <henry.chan@trefis.com>
2015-12-31 21:56:10 -0800

    a

--- html/src/com/suitecompiletech/tachyonsjourney/font/FontHelper.java
+++ html/src/com/suitecompiletech/tachyonsjourney/font/FontHelper.java
@@ -3,6 +3,7 @@ package com.suitecompiletech.tachyonsjourney.font;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 /*
  * Note the impl for this under the html project is different (damn GWT! - it doesn't support TTF)
  * FYI, to convert .ttf to .fnt, use http://kvazars.com/littera/
@@ -11,7 +12,10 @@ public class FontHelper {
 	public static BitmapFont getTTF(FontParam fontParam) {
 		BitmapFont retval = null;
 		retval = new BitmapFont(Gdx.files.internal("fonts/StoneyBilly.fnt"));
-		retval.setColor(Color.YELLOW);
+	
+		GlyphLayout glyphLayout = new GlyphLayout();
+		glyphLayout.setText(retval, fontParam.getText());
+		retval.setColor(fontParam.getColor());
 		return retval;
 	}
 }
