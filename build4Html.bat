b311ab92e3037d453d80d6ad9db67a93fc1f3747
Henry Chan <henry.chan@trefis.com>
2015-12-31 17:05:09 -0800

    a

--- build4Html.bat
+++ build4Html.bat
@@ -4,3 +4,4 @@ gradlew html:dist
 @rem to deploy to github pages
 @rem switch to 'gh-pages' branch and merge with master
 @http://hchan.github.io/tachyonsJourney/html/build/dist/
+cp -R html/build/dist/assets/* html/build/dist/html
