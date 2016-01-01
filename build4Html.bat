fe8e2db635734353b8948906f85923733922902f
Henry Chan <henry.chan@trefis.com>
2015-12-31 17:27:17 -0800

    a

--- build4Html.bat
+++ build4Html.bat
@@ -4,4 +4,4 @@ gradlew html:dist
 @rem to deploy to github pages
 @rem switch to 'gh-pages' branch and merge with master
 @http://hchan.github.io/tachyonsJourney/html/build/dist/
-cp -R html/build/dist/assets/* html/build/dist/html
+xcopy /s html\build\dist\assets\* html\build\dist\html
--- html/build/dist/html/blender/convert2G3DJ.bat
+++
@@ -1,1 +0,0 @@
-c:\fbx-conv\fbx-conv-win32.exe -f -o G3DJ sphere.fbx sphere.g3dj
